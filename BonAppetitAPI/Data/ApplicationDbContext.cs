06dba7b8fabf1208728e12f28fc71c275ba4c80e
Lucas Blotta <lucas@blotta.info>
2023-08-18 00:47:24 -0300

    add entity framework and identity

---
+++ BonAppetitAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BonAppetitAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BonAppetitAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<ApplicationUser> Users { get; set; }
+    }
+}
